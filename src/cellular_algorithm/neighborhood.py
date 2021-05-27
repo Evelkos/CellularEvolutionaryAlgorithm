6d15123a0129453b4178830ea954951e1c9c576f
Evelkos <ewelkos.chmielewska@gmail.com>
2021-05-27 21:41:22 +0200

    LinearNeighborhood.get_neighbours() implementation

--- src/cellular_algorithm/neighborhood.py
+++ src/cellular_algorithm/neighborhood.py
@@ -3,41 +3,53 @@ from abc import ABC, abstractmethod
 
 class Neighborhood(ABC):
     @abstractmethod
-    def get_neighbours(self, grid, cell_loc):
-        """Get list of cell's neighbours.
+    def get_neighbours(self, grid_shape, idx, distance):
+        """Get list of neighbours' positions on the grid.
 
         Arguments:
-            grid: grid that we want to get individuals from
-            cell_loc: individual's position on the grid
+            grid_shape: shape of the grid that we want to get individuals from
+            idx: individual's position on the grid
+            distance: max distance from the individual
 
         Return:
-            List of cell's neighbours.
+            Set of neighbours' positions.
 
         """
-        ...
+        pass
 
 
 class LinearNeighborhood(Neighborhood):
-    def __init__(self, distance=1):
-        """Init LinearNeighborhood.
+    @staticmethod
+    def get_neighbours(grid_shape, idx, distance=1):
+        """Get list of neighbours' positions on the grid.
 
         Arguments:
-            distance: max distance from the cell
-                distance = 1 => 5 individuals in the neighborhood
-                distance = 2 => 9 individuals in the neighborhood
+            grid_shape: shape of the grid that we want to get individuals from
+            idx: individual's position on the grid
+            distance: max distance from the individual
+
+        Return:
+            Set of neighbours' positions.
 
         """
-        self.distance = distance
+        assert len(grid_shape) == len(idx)
+
+        result = set()
+
+        # idx = (11, 24, 13) => position_0 = 11, position_1 = 24, etc.
+        for axis, position in enumerate(idx):
+            first_neighbor_margin = max(position - distance, 0)
+            last_neighbor_margin = min(position + distance, grid_shape[axis])
+
+            for neighbor_position in range(
+                first_neighbor_margin, last_neighbor_margin + 1
+            ):
+                neighbor_idx = list(idx)
+                neighbor_idx[axis] = neighbor_position
+                result.add(tuple(neighbor_idx))
+
+        return result
 
 
 class CompactNeighborhood(Neighborhood):
-    def __init__(self, distance=1):
-        """Init CompactNeighborhood.
-
-        Arguments:
-            distance: max distance from the cell
-                distance = 1 => 9 individuals in the neighborhood
-                distance = 2 => 13 individuals in the neighborhood
-
-        """
-        self.distance = distance
+    ...
