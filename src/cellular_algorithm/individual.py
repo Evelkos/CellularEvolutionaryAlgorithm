702ad08214db0bc5d01d6dd056a5c1bde3bbc993
Evelkos <ewelkos.chmielewska@gmail.com>
2021-05-29 21:42:25 +0200

    __init__ and __repr__ for Individual

--- src/cellular_algorithm/individual.py
+++ src/cellular_algorithm/individual.py
@@ -1,2 +1,7 @@
 class Individual:
-    ...
+    def __init__(self, coordinates, fitness=None):
+        self.coordinates = coordinates
+        self.fitness = fitness
+
+    def __repr__(self):
+        return str(self.coordinates)
