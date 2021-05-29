9fbc1a996a03073a18c65e4e6716e6d31463b21a
Evelkos <ewelkos.chmielewska@gmail.com>
2021-05-29 21:44:45 +0200

    Grid(ABC) deleted, Grid2D renamed -> Grid, generate_individuals() method added

--- src/cellular_algorithm/grid.py
+++ src/cellular_algorithm/grid.py
@@ -1,15 +1,9 @@
-from abc import ABC, abstractmethod
-
 import numpy as np
 
-from .individual import Individual
+from cellular_algorithm import Individual
 
 
-class Grid(ABC):
-    ...
-
-
-class Grid2D(Grid):
+class Grid:
     def __init__(self, shape, neighbourhood):
         """
         Arguments:
@@ -20,6 +14,50 @@ class Grid2D(Grid):
         self.grid = np.empty(shape, dtype=Individual)
         self.neighbourhood = neighbourhood
 
+    def __repr__(self):
+        return f"{self.grid}, {self.neighbourhood}"
+
+    @property
+    def shape(self):
+        return self.grid.shape
+
+    def generate_individuals(self, boundaries):
+        """Fill grid with random individuals.
+
+        Split grid into discrits of equal size. Generate individual inside each discrit.
+
+        Arguments:
+            boundaries: todo
+        """
+        # Compute step for each grid dimension.
+        steps = [
+            (max(boundary) - min(boundary)) / individuals
+            for individuals, boundary in zip(self.grid.shape, boundaries)
+        ]
+        # Get minimal possible values for individuals' coordinates.
+        min_vals = [min(boundary) for boundary in boundaries]
+
+        # For each position on the grid, compute coresponding discrit.
+        for grid_position, _ in self.get_individuals():
+            low = [
+                idx * step + min_val
+                for idx, step, min_val in zip(grid_position, steps, min_vals)
+            ]
+            high = [low_val + step for low_val, step in zip(low, steps)]
+
+            individual = Individual(coordinates=np.random.uniform(low=low, high=high))
+            self.set_individual(individual, grid_position)
+
+    def set_individual(self, individual, grid_position):
+        """Set new individual on the given position.
+
+        Arguments:
+            individual: individual we want to set on the given position
+            grid_position: individual's position on the grid
+
+        """
+        self.grid[grid_position] = individual
+
     def get_individuals(self):
-        for position, individual in np.ndenumerate(self.grid):
-            yield position, individual
+        for grid_position, individual in np.ndenumerate(self.grid):
+            yield grid_position, individual
