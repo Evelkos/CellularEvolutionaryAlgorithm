8b3c7cd098ec7f3d97976c7d6f27b13e228422cb
Evelkos <ewelkos.chmielewska@gmail.com>
2021-05-30 22:11:36 +0200

    new classes added to the __init__.py file

--- src/cellular_algorithm/__init__.py
+++ src/cellular_algorithm/__init__.py
@@ -1,4 +1,7 @@
-from .evolution import Evolution
-from .grid import Grid
+from .selection import TournamentSelection
 from .individual import Individual
+from .mutation import GaussianMutation
+from .crossover import UniformCrossover, SinglePointCrossover
 from .neighborhood import CompactNeighborhood, LinearNeighborhood
+from .grid import Grid
+from .evolution import Evolution
