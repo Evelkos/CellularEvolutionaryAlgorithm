0b95f749da0f77907fc414626e9a2186494a24d0
Ewelina <ewelkos.chmielewska@gmail.com>
2021-06-05 13:49:26 +0200

    main.py modified to use modified record() function

--- src/main.py
+++ src/main.py
@@ -36,45 +36,44 @@ def get_minimum(individual_1, individual_2):
 
 
 if __name__ == "__main__":
-    neighbourhood = CompactNeighborhood(distance=1)
+    neighbourhood = CompactNeighborhood(distance=2)
     selection = TournamentSelection(tournament_size=2, parents_num=2)
     crossover = UniformCrossover
-    mutation = GaussianMutation(scale=1)
+    mutation = GaussianMutation(scale=2)
 
-    boundaries = ((0, 10), (10, 20))
-    shape = (10, 10)
-    # evolution = CellularEvolutionaryAlgorithm(
-    #     neighbourhood=neighbourhood,
-    #     crossover=crossover,
-    #     mutation=mutation,
-    #     selection=selection,
-    #     boundaries=boundaries,
-    #     function=my_function,
-    #     maximize=False,
-    #     population_shape=shape,
-    #     mutation_probability=1,
-    #     iterations=10,
-    # )
-    # print(evolution.run())
-
-    selection = TournamentSelection(tournament_size=3, parents_num=2)
-    evolution = EvolutionaryAlgorithm(
+    boundaries = ((-100, 100), (-100, 100))
+    shape = (20, 20)
+    evolution = CellularEvolutionaryAlgorithm(
+        neighbourhood=neighbourhood,
         crossover=crossover,
         mutation=mutation,
         selection=selection,
         boundaries=boundaries,
-        function=my_function,
+        function=functions.f6,
         maximize=False,
+        population_shape=shape,
         mutation_probability=1,
-        iterations=100,
-        population_shape=(1, 100),
+        iterations=200,
     )
+    evolution_trace = evolution.run(save_trace=True)
+
+    # evolution = EvolutionaryAlgorithm(
+    #     crossover=crossover,
+    #     mutation=mutation,
+    #     selection=selection,
+    #     boundaries=boundaries,
+    #     function=functions.f5,
+    #     maximize=False,
+    #     mutation_probability=1,
+    #     iterations=200,
+    #     population_shape=(1, 100),
+    # )
 
     # plot_population_on_the_surface(
-    #     my_function,
+    #     functions.f5,
     #     points=120,
-    #     population_coordinates=population_coordinates,
+    #     population_coordinates=None,
     #     boundaries=boundaries,
     # )
 
-    record(evolution)
+    record(evolution_trace, evolution, points=20, iteration_step=10, filename=None)
